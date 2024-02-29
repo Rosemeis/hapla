5055bec1e8bfb965ff8da4537d12350df69ab791
Jonas Meisner <meisnerucph@gmail.com>
2024-02-29 17:17:37 +0100

    K-1 is back

--- hapla/struct.py
+++ hapla/struct.py
@@ -73,7 +73,7 @@ def main(args):
 					"Number of samples differ between cluster files!"
 
 			# Count haplotype cluster alleles
-			K_vec = np.max(Z_mat, axis=1) + 1
+			K_vec = np.max(Z_mat, axis=1) # Remove one cluster
 			m = np.sum(K_vec, dtype=int)
 			W = Z_mat.shape[0]
 
